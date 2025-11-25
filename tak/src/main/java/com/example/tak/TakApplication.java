1984b470b743ba2cea3ea23ebbf051932ae6c797
jeonggwwon <gyowon1026@gmail.com>
2025-11-25 20:29:03 +0900

    update

---
+++ tak/src/main/java/com/example/tak/TakApplication.java
@@ -0,0 +1,13 @@
+package com.example.tak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakApplication.class, args);
+	}
+
+}
