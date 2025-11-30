beaf373fc39f90f3065d0ea9a91452e9a1d05cc1
jeonggwwon <gyowon1026@gmail.com>
2025-12-01 03:36:03 +0900

    feat: Add WebClient configuration for AI Agent communication

---
+++ tak/src/main/java/com/example/tak/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.tak.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        log.info("SecurityConfig 초기화 시작");
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 기능 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 페이지 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/ta-kiosk/ai-agent/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+}
