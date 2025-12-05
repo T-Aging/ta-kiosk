eb64873ba2e47cee9ba34cc8bef464e5bda2a053
jeonggwwon <gyowon1026@gmail.com>
2025-12-06 02:46:55 +0900

    feat: 초기 메뉴 데이터에 S3 이미지 URL 적용

--- tak/src/main/resources/data.sql
+++ tak/src/main/resources/data.sql
@@ -23,54 +23,54 @@ VALUES
 INSERT INTO menu (menu_id, menu_name, menu_price, description, calorie, sugar, caffeine, allergic, menu_image)
 VALUES
   -- 아메리카노 계열
-  (1,  '아메리카노',           2000, '기본 에스프레소 커피',                     5,   0,   120, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (3,  '연유 아메리카노',      2500, '연유가 들어가 달달한 아메리카노',          80,  15,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (4,  '디카페인 아메리카노',  2500, '카페인을 줄인 아메리카노',                 5,   0,    5, 'NONE', 'http://localhost:8080/image/screen.png'),
+  (1,  '아메리카노',           2000, '기본 에스프레소 커피',                     5,   0,   120, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-americano.jpg'),
+  (3,  '연유 아메리카노',      2500, '연유가 들어가 달달한 아메리카노',          80,  15,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-americano.jpg'),
+  (4,  '디카페인 아메리카노',  2500, '카페인을 줄인 아메리카노',                 5,   0,    5, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-americano.jpg'),
 
-  (5,  '콜드브루',             3000, '진하게 우려낸 콜드브루 커피',              10,  0,   150, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (6,  '디카페인 콜드브루',    3500, '카페인을 줄인 콜드브루',                   10,  0,    5, 'NONE', 'http://localhost:8080/image/screen.png'),
+  (5,  '콜드브루',             3000, '진하게 우려낸 콜드브루 커피',              10,  0,   150, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/ice-americano.jpg'),
+  (6,  '디카페인 콜드브루',    3500, '카페인을 줄인 콜드브루',                   10,  0,    5, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/ice-americano.jpg'),
 
   -- 라떼 계열
-  (10, '카페라떼',             2800, '우유가 들어간 부드러운 라떼',              180, 10,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (11, '디카페인 라떼',        3300, '카페인을 줄인 부드러운 라떼',              180, 10,    5, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (12, '바닐라라떼',           3200, '바닐라 시럽이 들어간 달콤한 라떼',         220, 25,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (13, '카라멜라떼',           3300, '카라멜 시럽이 들어간 달콤한 라떼',         230, 26,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (14, '헤이즐넛라떼',         3200, '헤이즐넛 향이 나는 고소한 라떼',           220, 20,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (15, '카페모카',             3200, '초콜릿이 들어간 달콤한 모카 라떼',         250, 28,  120, 'MILK', 'http://localhost:8080/image/screen.png'),
+  (10, '카페라떼',             2800, '우유가 들어간 부드러운 라떼',              180, 10,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (11, '디카페인 라떼',        3300, '카페인을 줄인 부드러운 라떼',              180, 10,    5, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (12, '바닐라라떼',           3200, '바닐라 시럽이 들어간 달콤한 라떼',         220, 25,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (13, '카라멜라떼',           3300, '카라멜 시럽이 들어간 달콤한 라떼',         230, 26,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (14, '헤이즐넛라떼',         3200, '헤이즐넛 향이 나는 고소한 라떼',           220, 20,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (15, '카페모카',             3200, '초콜릿이 들어간 달콤한 모카 라떼',         250, 28,  120, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
 
   -- 논커피 라떼 / 기타 라떼
-  (20, '초코라떼',             3000, '달콤한 초콜릿이 들어간 논커피 라떼',       260, 30,   10, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (21, '화이트초코라떼',       3200, '화이트 초콜릿이 들어간 부드러운 라떼',     270, 32,   10, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (22, '녹차라떼',             3200, '녹차와 우유가 어우러진 라떼',              240, 20,   30, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (23, '고구마라떼',           3500, '고구마가 들어간 포만감 있는 라떼',         280, 35,   10, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (24, '밀크티라떼',           3300, '홍차와 우유가 들어간 밀크티 라떼',          230, 25,   40, 'MILK', 'http://localhost:8080/image/screen.png'),
+  (20, '초코라떼',             3000, '달콤한 초콜릿이 들어간 논커피 라떼',       260, 30,   10, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/non-coffee/choco.jpg'),
+  (21, '화이트초코라떼',       3200, '화이트 초콜릿이 들어간 부드러운 라떼',     270, 32,   10, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/ice-latte.jpg'),
+  (22, '녹차라떼',             3200, '녹차와 우유가 어우러진 라떼',              240, 20,   30, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/non-coffee/green.jpg'),
+  (23, '고구마라떼',           3500, '고구마가 들어간 포만감 있는 라떼',         280, 35,   10, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/hot-latte.jpg'),
+  (24, '밀크티라떼',           3300, '홍차와 우유가 들어간 밀크티 라떼',          230, 25,   40, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/ice-latte.jpg'),
 
   -- 에이드
-  (30, '레몬에이드',           3000, '상큼한 레몬이 들어간 에이드',              140, 30,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (31, '자몽에이드',           3500, '자몽이 들어간 상큼한 에이드',              150, 32,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (32, '청포도에이드',         3300, '청포도가 들어간 달콤한 에이드',            150, 32,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (33, '오렌지에이드',         3300, '오렌지가 들어간 상큼한 에이드',            150, 32,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
+  (30, '레몬에이드',           3000, '상큼한 레몬이 들어간 에이드',              140, 30,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/lemon.jpg'),
+  (31, '자몽에이드',           3500, '자몽이 들어간 상큼한 에이드',              150, 32,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/grapefruit.jpg'),
+  (32, '청포도에이드',         3300, '청포도가 들어간 달콤한 에이드',            150, 32,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/grape.jpg'),
+  (33, '오렌지에이드',         3300, '오렌지가 들어간 상큼한 에이드',            150, 32,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/orange-ade.jpg'),
 
   -- 티
-  (40, '얼그레이티',           2500, '향긋한 얼그레이 홍차',                     5,   0,   30, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (41, '캐모마일티',           2500, '은은한 꽃향의 캐모마일 티',                5,   0,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (42, '페퍼민트티',           2500, '상쾌한 페퍼민트 허브티',                   5,   0,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (43, '복숭아 아이스티',      2500, '달달한 복숭아 향의 아이스티',              120, 28,   0, 'NONE', 'http://localhost:8080/image/screen.png'),
+  (40, '얼그레이티',           2500, '향긋한 얼그레이 홍차',                     5,   0,   30, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/tea/tea.jpg'),
+  (41, '캐모마일티',           2500, '은은한 꽃향의 캐모마일 티',                5,   0,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/tea/tea.jpg'),
+  (42, '페퍼민트티',           2500, '상쾌한 페퍼민트 허브티',                   5,   0,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/tea/tea.jpg'),
+  (43, '복숭아 아이스티',      2500, '달달한 복숭아 향의 아이스티',              120, 28,   0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/tea/tea.jpg'),
 
   -- 스무디
-  (50, '딸기 스무디',          3800, '딸기가 들어간 달콤한 스무디',              250, 35,    0, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (51, '망고 스무디',          3800, '망고가 들어간 달콤한 스무디',              250, 35,    0, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (52, '요거트 스무디',        3800, '상큼한 요거트 베이스 스무디',              230, 30,    0, 'MILK', 'http://localhost:8080/image/screen.png'),
+  (50, '딸기 스무디',          3800, '딸기가 들어간 달콤한 스무디',              250, 35,    0, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/berry.jpg'),
+  (51, '망고 스무디',          3800, '망고가 들어간 달콤한 스무디',              250, 35,    0, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/mango.jpg'),
+  (52, '요거트 스무디',        3800, '상큼한 요거트 베이스 스무디',              230, 30,    0, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/yogurt.jpg'),
 
   -- 프라페
-  (60, '모카프라페',           4200, '모카 베이스의 달콤한 프라페',              300, 40,   80, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (61, '카라멜프라페',         4300, '카라멜 소스가 듬뿍 들어간 프라페',         310, 42,   80, 'MILK', 'http://localhost:8080/image/screen.png'),
-  (62, '쿠키앤크림프라페',     4500, '쿠키와 크림이 들어간 프라페',              320, 45,   80, 'MILK', 'http://localhost:8080/image/screen.png'),
+  (60, '모카프라페',           4200, '모카 베이스의 달콤한 프라페',              300, 40,   80, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/coffee/coffee.jpg'),
+  (61, '카라멜프라페',         4300, '카라멜 소스가 듬뿍 들어간 프라페',         310, 42,   80, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/non-coffee/caramel.jpg'),
+  (62, '쿠키앤크림프라페',     4500, '쿠키와 크림이 들어간 프라페',              320, 45,   80, 'MILK', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/non-coffee/cookie.jpg'),
 
   -- 주스
-  (70, '오렌지 주스',          3500, '상큼한 오렌지 생과일 주스',                140, 30,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (71, '포도 주스',            3500, '달콤한 포도 생과일 주스',                  140, 30,    0, 'NONE', 'http://localhost:8080/image/screen.png'),
-  (72, '토마토 주스',          3500, '담백한 토마토 주스',                       80,  15,    0, 'NONE', 'http://localhost:8080/image/screen.png');
+  (70, '오렌지 주스',          3500, '상큼한 오렌지 생과일 주스',                140, 30,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/orange-juice.jpg'),
+  (71, '포도 주스',            3500, '달콤한 포도 생과일 주스',                  140, 30,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/grape.jpg'),
+  (72, '토마토 주스',          3500, '담백한 토마토 주스',                       80,  15,    0, 'NONE', 'https://inha-capstone-23-taging-kiosk.s3.us-west-2.amazonaws.com/fruit/tomato.jpg');
 
 -- ===== 매장-메뉴 매핑 =====
 INSERT INTO store_menu_mapping (store_id, menu_id)
